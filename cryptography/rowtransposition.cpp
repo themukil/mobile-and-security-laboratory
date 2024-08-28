f7512b50986bfed99fa509c8e79cb2b877e44e7e
Mukilarasan V <mukilarasan.v@gmail.com>
2024-08-28 09:40:29 +0530

    Update rowtransposition.cpp

--- cryptography/rowtransposition.cpp
+++ cryptography/rowtransposition.cpp
@@ -1,64 +1,106 @@
-#include <iostream>
-#include <string>
-#include <vector>
 
+#include <bits/stdc++.h>
 using namespace std;
 
-string encryptRowColumnTransposition(const string& text, int rows, int cols) {
-    vector<vector<char>> matrix(rows, vector<char>(cols, ' '));
-
-    int index = 0;
-    for (int r = 0; r < rows; ++r) {
-        for (int c = 0; c < cols && index < text.length(); ++c) {
-            matrix[r][c] = text[index++];
+string Encryption(int no_rows, int len_key, int len_msg,
+                  string msg, int col_val[])
+{
+    int x = 0;
+    char enc_mat[no_rows + 1][len_key];
+    for (int i = 0; i < no_rows + 1; i++) {
+        for (int j = 0; j < len_key; j++) {
+            if (x >= len_msg) {
+                enc_mat[i][j] = '_';
+            }
+            else {
+                enc_mat[i][j] = msg[x];
+            }
+            x++;
         }
     }
 
-    string encryptedText;
-    for (int c = 0; c < cols; ++c) {
-        for (int r = 0; r < rows; ++r) {
-            if (matrix[r][c] != ' ') {
-                encryptedText += matrix[r][c];
+    int t = 1;
+    string cipher = "";
+    while (t <= len_key) {
+        for (int i = 0; i < len_key; i++) {
+            int k = col_val[i];
+            if (k == t) {
+                for (int j = 0; j < no_rows + 1; j++) {
+                    cipher += enc_mat[j][i];
+                }
+                t++;
+            }
+        }
+    }
+    return cipher;
+}
+
+string Decryption(int no_rows, int len_key, string cipher,
+                  int col_val[])
+{
+    char dec_mat[no_rows + 1][len_key];
+    int x = 0, t = 1;
+    while (t <= len_key) {
+        for (int i = 0; i < len_key; i++) {
+            int k = col_val[i];
+            if (k == t) {
+                for (int j = 0; j < no_rows + 1; j++) {
+                    dec_mat[j][i] = cipher[x];
+                    x++;
+                }
+                t++;
             }
         }
     }
 
-    return encryptedText;
-}
-
-string decryptRowColumnTransposition(const string& text, int rows, int cols) {
-    vector<vector<char>> matrix(rows, vector<char>(cols, ' '));
-
-    int index = 0;
-    for (int c = 0; c < cols; ++c) {
-        for (int r = 0; r < rows && index < text.length(); ++r) {
-            matrix[r][c] = text[index++];
+    string message = "";
+    for (int i = 0; i < no_rows + 1; i++) {
+        for (int j = 0; j < len_key; j++) {
+            if (dec_mat[i][j] == '_') {
+                dec_mat[i][j] = ' ';
+            }
+            message += dec_mat[i][j];
         }
     }
+    return message;
+}
 
-    string decryptedText;
-    for (int r = 0; r < rows; ++r) {
-        for (int c = 0; c < cols; ++c) {
-            if (matrix[r][c] != ' ') {
-                decryptedText += matrix[r][c];
+int main()
+{
+    string msg = "Geeks for Geeks";
+
+    string key = "HACK";
+
+    int len_key = key.length();
+    int len_msg = msg.length();
+
+    int val = 1, count = 0, ind;
+
+    int col_val[len_key];
+    memset(col_val, 0, sizeof(col_val));
+    while (count < len_key) {
+        int min = 999;
+        for (int i = 0; i < len_key; i++) {
+            if ((min > int(key[i])) && (col_val[i] == 0)) {
+                min = int(key[i]);
+                ind = i;
             }
         }
+        col_val[ind] = val;
+        count++;
+        val++;
     }
 
-    return decryptedText;
+    int no_rows = len_msg / len_key;
+    // encrypted text
+    string cipher_text = " ";
+    cipher_text = Encryption(no_rows, len_key, len_msg, msg,
+                             col_val);
+    cout << "Encrypted Message : " << cipher_text << endl;
+    // decrypted text
+    string original_msg = " ";
+    original_msg = Decryption(no_rows, len_key, cipher_text,
+                              col_val);
+    cout << "Decrypted Message : " << original_msg << endl;
 }
 
-int main() {
-    string plaintext = "GUINDY";
-    int rows = 2;
-    int cols = 3;
-
-    string ciphertext = encryptRowColumnTransposition(plaintext, rows, cols);
-    string decryptedText = decryptRowColumnTransposition(ciphertext, rows, cols);
-
-    cout << "Plaintext: " << plaintext << endl;
-    cout << "Ciphertext: " << ciphertext << endl;
-    cout << "Decrypted Text: " << decryptedText << endl;
-
-    return 0;
-}
