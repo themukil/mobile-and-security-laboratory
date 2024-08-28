5c8c23b19ac009306d4cf2034faaae4ad05f5cdf
Mukilarasan V <mukilarasan.v@gmail.com>
2024-08-28 09:35:19 +0530

    Update hillcipher.cpp

--- cryptography/hillcipher.cpp
+++ cryptography/hillcipher.cpp
@@ -1,83 +1,70 @@
-#include<iostream>
-#include <string>
+#include <iostream>
 using namespace std;
 
-int main(){
-	string letter;
-	int k=0;
-	int lettermatrix[3][1];
-	
-	cout<<"enter the letter=";
-	getline(cin,letter);
-	
-	for(int i=0;i<3;i++){
-		for(int j=0;j<1;j++){
-			lettermatrix[i][j]=(letter[k])%65;
-            k++;
-		}
-	}
-	cout<<"letter to number=";
-	 for(int i=0;i<3;i++)
-    {
-        for ( int j = 0; j<1; j++)
-        {
-            cout<<lettermatrix[i][j];
-        }
-    } 
-	string key;
-	k=0;
-	int keymatrix[3][3];
-    cout<<"\nEnter the key =";
-    getline(cin,key);
- 
-    for(int i=0;i<3;i++)
-    {
-        for ( int j = 0; j<3; j++)
-        {
-            keymatrix[i][j]=(key[k])%65;
-            k++;
-        }
-    }
-     for(int i=0;i<3;i++)
-    {
-        for (int  j = 0; j<3; j++)
-        {
-            cout<<keymatrix[i][j];
-        }
-    }
-    string choice;
-    cout<<"enter your choice \n1. encryption\n2. decryption";
-    cin>>choice;
-    
-    if(choice==1){
-    int sum;
-    int multmatrix[3][1];
-    
-    int result[3][1];
-    
-    for (int i = 0; i < 3; i++) {
-        sum = 0;
-        for (int j = 0; j < 3; j++) {
-            sum += keymatrix[i][j] * lettermatrix[j][0];
-        }
-        result[i][0] = sum % 26;
-    }
-    
-    for(int i=0;i<3;i++){
-    	for(int j=0;j<1;j++){
-    		cout<<result[i][j];
+void getKeyMatrix(string key, int keyMatrix[][3])
+{
+	int k = 0;
+	for (int i = 0; i < 3; i++) 
+	{
+		for (int j = 0; j < 3; j++) 
+		{
+			keyMatrix[i][j] = (key[k]) % 65;
+			k++;
 		}
 	}
 }
 
-	string ans[3][1];
-	char ch;
-	for(int i=0;i<3;i++){
-		for(int j=0;j<1;j++){
-			ans[i][j]=result[i][j]+'A';
+void encrypt(int cipherMatrix[][1],
+			int keyMatrix[][3], 
+			int messageVector[][1])
+{
+	int x, i, j;
+	for (i = 0; i < 3; i++) 
+	{
+		for (j = 0; j < 1; j++)
+		{
+			cipherMatrix[i][j] = 0;
+		
+			for (x = 0; x < 3; x++)
+			{
+				cipherMatrix[i][j] += 
+					keyMatrix[i][x] * messageVector[x][j];
+			}
+		
+			cipherMatrix[i][j] = cipherMatrix[i][j] % 26;
 		}
 	}
-   
 }
 
+void HillCipher(string message, string key)
+{
+	int keyMatrix[3][3];
+	getKeyMatrix(key, keyMatrix);
 
+	int messageVector[3][1];
+
+	for (int i = 0; i < 3; i++)
+		messageVector[i][0] = (message[i]) % 65;
+
+	int cipherMatrix[3][1];
+
+	encrypt(cipherMatrix, keyMatrix, messageVector);
+
+	string CipherText;
+
+	for (int i = 0; i < 3; i++)
+		CipherText += cipherMatrix[i][0] + 65;
+
+	cout << " Ciphertext:" << CipherText;
+}
+
+int main()
+{
+	string message = "ACT";
+
+	string key = "GYBNQKURP";
+
+	HillCipher(message, key);
+
+	return 0;
+}
